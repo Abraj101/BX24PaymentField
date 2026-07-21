253a204afce93a67c69b40e998f74ebf61b9cc21
iAmHammad261 <hammad2nd@gmail.com>
2026-07-22 00:04:09 +0500

    Feat: changes the userid method to the bx24 promise

--- public/refreshApp.js
+++ public/refreshApp.js
@@ -1,33 +1,38 @@
-const getCurrentUserId = () => {
-  let userId = null;
-  BX24.callMethod("profile", {}, (result) => {
-    if (result.error()) {
-      console.error("Error fetching current user ID:", result.error());
-    } else {
-      userId = result.data().ID;
-      console.log("Current user ID:", userId);
-    }
+function callBX(method, params) {
+  return new Promise(function (resolve, reject) {
+    BX24.callMethod(method, params, function (res) {
+      if (res.error()) reject(res.error());
+      else resolve(res.data());
+    });
   });
-
-  return userId;
-};
-
-try {
-  window.appPullClient = new BX.PullClient({
-    restApplication: "dxbPaymentPlan.bitrix24.com",
-    restClient: BX24,
-    userId: getCurrentUserId(),
-  });
-
-  window.appPullClient.subscribe({
-    moduleId: "application",
-    callback: function (data) {
-      console.log("PullClient callback received data:", data);
-      console.warn(data);
-    }.bind(this),
-  });
-
-  window.appPullClient.start();
-} catch (e) {
-  console.error("Error initializing PullClient:", e);
 }
+
+async function getCurrentUserId() {
+  const profile = await callBX("profile", {});
+  return profile.ID;
+}
+
+(async () => {
+  try {
+    const userId = await getCurrentUserId();
+    console.log("Current user ID:", userId);
+
+    window.appPullClient = new BX.PullClient({
+      restApplication: "dxbPaymentPlan.bitrix24.com",
+      restClient: BX24,
+      userId: userId,
+    });
+
+    window.appPullClient.subscribe({
+      moduleId: "application",
+      callback: function (data) {
+        console.log("PullClient callback received data:", data);
+        console.warn(data);
+      },
+    });
+
+    window.appPullClient.start();
+  } catch (e) {
+    console.error("Error initializing PullClient:", e);
+  }
+})();
