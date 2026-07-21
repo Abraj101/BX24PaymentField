4a512e9fc19c841c8be318de5aabc61f86c0eb79
iAmHammad261 <hammad2nd@gmail.com>
2026-07-21 17:42:15 +0500

    Feat: add getting of the current user id

--- public/refreshApp.js
+++ public/refreshApp.js
@@ -1,8 +1,22 @@
+const getCurrentUserId = () => {
+  let userId = null;
+  BX24.callMethod("profile", {}, (result) => {
+    if (result.error()) {
+      console.error("Error fetching current user ID:", result.error());
+    } else {
+      userId = result.data().ID;
+      console.log("Current user ID:", userId);
+    }
+  });
+
+  return userId;
+};
+
 try {
   window.appPullClient = new BX.PullClient({
     restApplication: "dxbPaymentPlan.bitrix24.com",
     restClient: BX24,
-    userId: 1,
+    userId: getCurrentUserId(),
   });
 
   window.appPullClient.subscribe({
